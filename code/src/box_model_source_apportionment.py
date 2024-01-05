6463c0aaffaecd77fa01d7b547e130279922bdca
shenglun7 <shenglunwu@gmail.com>
2024-01-04 16:36:01 -0800

    function for plot VOC source concentrations

--- code/src/box_model_source_apportionment.py
+++ code/src/box_model_source_apportionment.py
@@ -47,3 +47,37 @@ def treat_box_model_output(filepath):
     return df
 
 
+def get_initial_conc(filepath):
+    """
+    get initial concentration of VOCs
+    input: filepath = location of the output file
+    output: df = df of initial concentration ['Date','Time','O3','VOCs'], where Time = time in UV reaction, O3 = vanilla O3, VOCs = vanilla VOCs
+    """
+
+    para_number = 1147  # number of parameters in the box model
+    df = pd.read_csv(filepath, skiprows=1, nrows=para_number, 
+                     header=None,delim_whitespace=True)
+    df.columns = ['Number','Parameter','Value']
+
+    return df
+
+
+def get_daily_source_conc(date, df, list_saprc):
+    """
+    get daily concentration of every source of VOCs from a df of box model output
+    input: date = date of the box model output
+           df = df of box model output: ['Number','Parameter','Value']
+           list_saprc = list of SAPRC parameters name accounted for calculation
+    output: df_out = df:['Date','Factor','Conc']
+    """
+
+    df_source_conc = pd.DataFrame(columns=['Date','Factor','Conc'])
+    nspecies = 9
+    for i in range(nspecies):
+        list_saprc_X = [list_saprc + '_X' + str(i+1) for list_saprc in list_saprc]
+        sum_conc = df.loc[df['Parameter'].isin(list_saprc_X), 'Value'].sum()
+        df_source_conc = \
+            df_source_conc.append({'Date':date, 'Factor': 'X'+str(i+1), 'Conc': sum_conc},
+                                  ignore_index=True)
+        
+    return df_source_conc
